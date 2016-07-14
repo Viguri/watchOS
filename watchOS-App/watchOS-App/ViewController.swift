f63a11c9ba305034d3a8c5ac60176bdc481f718c
Viguri <manuel@viguri.org>
2016-07-14 21:49:01 +0200

    commit

---
+++ watchOS-App/watchOS-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchOS-App
+//
+//  Created by Manuel Rodríguez de Viguri on 14/7/16.
+//  Copyright © 2016 Manuel Rodríguez de Viguri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
